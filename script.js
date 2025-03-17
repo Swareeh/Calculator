3802ce29980aec38c8efe7c00aa643e96d748258
Swareeh <77015406+Swareeh@users.noreply.github.com>
2025-03-17 14:40:49 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const one_button = document.getElementById('1')
+const two_button = document.getElementById('2')
+const equal_button = document.getElementById('equal')
+const textarea = document.getElementById('textarea')
+const plus = document.getElementById('plus')
+
+// var contnet = textarea.textContent
+
+// one_button.addEventListener('click', function() {
+//     contnet = textarea.textContent
+//     textarea.innerHTML = contnet+'1'
+// })
+
+// two_button.addEventListener('click', function() {
+//     contnet = textarea.textContent
+//     textarea.innerHTML = contnet+'2'
+//     })
+
+// plus.addEventListener('click', function() {
+//     contnet = textarea.textContent
+//     textarea.innerHTML = contnet+'+'
+//     })
+
+// equal_button.addEventListener('click', function() {
+//     try {
+//         contnet = textarea.textContent
+//     textarea.innerHTML = eval(contnet)
+//     } catch {
+//         alert('Something went wrong!')
+//     }
+//     })
